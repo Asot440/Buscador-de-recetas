693053301569ea021958c55267c7b5969bf30a76
Jadri-PNZRKMPFWGN <asot_buuren440@hotmail.com>
2026-01-28 20:33:13 -0600

    Avance 2 completado

--- dist/Buscador-de-recetas.4a59a05f.js
+++ dist/Buscador-de-recetas.4a59a05f.js
@@ -830,9 +830,14 @@ async function loadRecipe(id) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "getJSON", ()=>getJSON);
+var _configJs = require("./config.js");
 async function getJSON(url) {
     try {
-        const res = await fetch(url);
+        const fetchPro = fetch(url);
+        const res = await Promise.race([
+            fetchPro,
+            timeout((0, _configJs.TIMEOUT_SEC))
+        ]);
         const data = await res.json();
         if (!res.ok) throw new Error(`Error ${res.status}: no se pudo obtener la receta`);
         return data;
@@ -840,12 +845,17 @@ async function getJSON(url) {
         throw err;
     }
 }
+const timeout = function(s) {
+    return new Promise((_, reject)=>setTimeout(()=>reject(new Error(`Request took too long! Timeout after ${s} second${s > 1 ? 's' : ''}`)), s * 1000));
+};
 
-},{"@parcel/transformer-js/src/esmodule-helpers.js":"jnFvT"}],"2hPh4":[function(require,module,exports,__globalThis) {
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"jnFvT","./config.js":"2hPh4"}],"2hPh4":[function(require,module,exports,__globalThis) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "API_URL", ()=>API_URL);
+parcelHelpers.export(exports, "TIMEOUT_SEC", ()=>TIMEOUT_SEC);
 const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+const TIMEOUT_SEC = 5;
 
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"jnFvT"}],"dfIpa":[function(require,module,exports,__globalThis) {
 //import { recipeContainer } from '../controller.js';
